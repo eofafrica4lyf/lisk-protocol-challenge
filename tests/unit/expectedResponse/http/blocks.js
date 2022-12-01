da8c1b1f7fce331390b4cde83e1cffffdd0d9292
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2022-12-01 12:31:37 +0100

    add dummy data for unit tests

---
+++ tests/unit/expectedResponse/http/blocks.js
@@ -0,0 +1,354 @@
+const transactionsArray = [
+	{
+		moduleID: 2,
+		assetID: 0,
+		nonce: "21869",
+		fee: "250000",
+		senderPublicKey:
+			"de4919066768d82221bd11e912f871e8917230622ef3c333ab6042ee1044af19",
+		asset: {
+			amount: "11148514",
+			recipientAddress: "8d14c38533ad7d5667ec013d10cd9f5b0a8737cc",
+			data: "loteria payouts",
+		},
+		signatures: [
+			"41457e5ca2033e0a541c4b54b6d77074bd559523d0eb6e6cec2e581d5b67c66d99ca42ed37e80149a230fe23bc01e28d3d702f41cd03e26321e144ef7a44eb07",
+		],
+		id: "0dcc3245f5fd832a61678801566d8e535db37719218e09e78c87778444bb1b75",
+	},
+	{
+		moduleID: 2,
+		assetID: 0,
+		nonce: "21870",
+		fee: "250000",
+		senderPublicKey:
+			"de4919066768d82221bd11e912f871e8917230622ef3c333ab6042ee1044af19",
+		asset: {
+			amount: "18116337",
+			recipientAddress: "b6472df3b833e13108fa61f801950b8b6a157510",
+			data: "loteria payouts",
+		},
+		signatures: [
+			"af40e74f8a084108c2dcae858c57494b6d94a4d52fea2fb051c7120149e4bc08bebbcaffd6273325b8cf1c93caf085ce36951b6fd613c412627285809a42b100",
+		],
+		id: "1bd1d685b8b5308cb11606d202b26f856805919ae09f5db4bd847591d45a0f23",
+	},
+	{
+		moduleID: 2,
+		assetID: 0,
+		nonce: "21871",
+		fee: "250000",
+		senderPublicKey:
+			"de4919066768d82221bd11e912f871e8917230622ef3c333ab6042ee1044af19",
+		asset: {
+			amount: "10576158",
+			recipientAddress: "baad2a36989b0c4d7d13f46cd27e5e0d3e59db69",
+			data: "loteria payouts",
+		},
+		signatures: [
+			"48c5427444cef785b9af86803c363ff2f8d67ac4b5d05322e7acf5fa9842e91f5ed9c8d7248a2480cae34881887017afc93d42efc238801456267a8582eaed07",
+		],
+		id: "ac9f3c6441bbf79e88510ffa7ef439f04a3f6acbd9c6f8e153db31ae28c65ac9",
+	},
+	{
+		moduleID: 2,
+		assetID: 0,
+		nonce: "21872",
+		fee: "250000",
+		senderPublicKey:
+			"de4919066768d82221bd11e912f871e8917230622ef3c333ab6042ee1044af19",
+		asset: {
+			amount: "312681021",
+			recipientAddress: "4d770a0620112ee15389ffb692ccc999a3648062",
+			data: "loteria payouts",
+		},
+		signatures: [
+			"3398994806495e2065b1c84c5f872917e90b264b0f265fbf8cc5b4da6e717ee16b6e2fdecbd2734e74620963913f3e5fb6a0052be738a4ade8fe25e548aa6707",
+		],
+		id: "3d16d75f1ae50120e51964e0aa4a156016d7620594c7719f474847a40bddc4c9",
+	},
+	{
+		moduleID: 2,
+		assetID: 0,
+		nonce: "21873",
+		fee: "250000",
+		senderPublicKey:
+			"de4919066768d82221bd11e912f871e8917230622ef3c333ab6042ee1044af19",
+		asset: {
+			amount: "13064666",
+			recipientAddress: "be1e2a1c6acfc7997baf0798f55c905e726245c5",
+			data: "loteria payouts",
+		},
+		signatures: [
+			"202a032427e01e73b0cc700ab69cba409f1a472645abbbe9032791e0c71292857e67ef9706769fd10cc514dbcce305be14ba69db496627a2cd878c6a49145609",
+		],
+		id: "19c103e267f6815806e28a63e260e6e89e035b0bd5d98b169f9f16b23768c9ef",
+	},
+	{
+		moduleID: 2,
+		assetID: 0,
+		nonce: "21874",
+		fee: "250000",
+		senderPublicKey:
+			"de4919066768d82221bd11e912f871e8917230622ef3c333ab6042ee1044af19",
+		asset: {
+			amount: "10223619",
+			recipientAddress: "b17bfbb252360717d5f22fb4298f30275f350b55",
+			data: "loteria payouts",
+		},
+		signatures: [
+			"3ae5b028ac4114c4f90d9f50e577cf2f366eda679f3175ee06750741d849fcdd3ca05dd2c35cec183c4f211479e1ad94f80e462f225dc65c0cb8b5574e63e703",
+		],
+		id: "0396262adf884c2adf583af9bd79356642e4266aadf32251b4c3b34c11b44c07",
+	},
+	{
+		moduleID: 2,
+		assetID: 0,
+		nonce: "21875",
+		fee: "250000",
+		senderPublicKey:
+			"de4919066768d82221bd11e912f871e8917230622ef3c333ab6042ee1044af19",
+		asset: {
+			amount: "34839111",
+			recipientAddress: "b7516c05a8a33ecabd84fca45fe34660b5694c8b",
+			data: "loteria payouts",
+		},
+		signatures: [
+			"48aa694c17546e9bb5268032dda512291c17b44b3b81e04b5b77bdb9337225810c561f911d77b1f329c3d020485c0d92b1a25c56e120521db9dd09619e316403",
+		],
+		id: "04e121d816f5014904e26e032009d5862a3953f3b17297187c165d3130f61743",
+	},
+	{
+		moduleID: 2,
+		assetID: 0,
+		nonce: "21876",
+		fee: "250000",
+		senderPublicKey:
+			"de4919066768d82221bd11e912f871e8917230622ef3c333ab6042ee1044af19",
+		asset: {
+			amount: "13935644",
+			recipientAddress: "16c16491349cf31601947ba43d5acc8ff93f5d0b",
+			data: "loteria payouts",
+		},
+		signatures: [
+			"3696c3456589f516c144a89b12465eed35cb9c4c92182936cee49d0d6efd13382ee822b837d5541cb5faacf0150575d8d85a195570301f2329a419b605a6000a",
+		],
+		id: "8888cc84215d3d227adb7a66684e86ba56b6398a1c4315c2b1971476988b59f9",
+	},
+	{
+		moduleID: 2,
+		assetID: 0,
+		nonce: "21877",
+		fee: "250000",
+		senderPublicKey:
+			"de4919066768d82221bd11e912f871e8917230622ef3c333ab6042ee1044af19",
+		asset: {
+			amount: "17419555",
+			recipientAddress: "bc415d455cf6da65f16b80515cd5fb0017b7c7c3",
+			data: "loteria payouts",
+		},
+		signatures: [
+			"75636a2c3ba42572acf138952ead3a857aad2d0103f37b8b9273851bbbafa5b13d3ebc02b9611028319fce12564cd87fd89ebb9a0e3c92afcb95954e2717fe02",
+		],
+		id: "f185c22af29ddf8375176f6f76ca02ee5fbac4476163053ed82676150bbea88a",
+	},
+	{
+		moduleID: 2,
+		assetID: 0,
+		nonce: "21878",
+		fee: "250000",
+		senderPublicKey:
+			"de4919066768d82221bd11e912f871e8917230622ef3c333ab6042ee1044af19",
+		asset: {
+			amount: "87097777",
+			recipientAddress: "ef93860e2196de35f10cdf1b8b17adff7129dae4",
+			data: "loteria payouts",
+		},
+		signatures: [
+			"cf27432b1066ff2119f4eb2ae8e5949bdc04285fbb8a168331c1bb3396d1aa66d4cbdac3cf080f9aa182fd097355dc708c735f0c714e548753a577d6ad0b7c0f",
+		],
+		id: "95094fb07017175136eff6b3b4a9f446d00cccda44ae7b0a386809540c4db7ed",
+	},
+];
+
+const totalTransferValue = 529102402;
+
+const currentBlockData = {
+	data: {
+		version: "3.0.4",
+		networkVersion: "3.1",
+		networkIdentifier:
+			"4c09e6a781fc4c7bdb936ee815de8f94190f8a7519becd9de2081832be309a99",
+		lastBlockID:
+			"9f024d700048cde04a6fac6a4d5d77a54b7ee9d233e473591765795b95c484c3",
+		height: 20247045,
+		genesisHeight: 16270293,
+		finalizedHeight: 20246875,
+		syncing: false,
+		unconfirmedTransactions: 0,
+		genesisConfig: {
+			blockTime: 10,
+			communityIdentifier: "Lisk",
+			maxPayloadLength: 15360,
+			bftThreshold: 68,
+			minFeePerByte: 1000,
+			baseFees: [
+				{
+					moduleID: 5,
+					assetID: 0,
+					baseFee: "1000000000",
+				},
+			],
+			roundLength: 103,
+			rewards: {
+				milestones: [
+					"500000000",
+					"400000000",
+					"300000000",
+					"200000000",
+					"100000000",
+				],
+				offset: 1451520,
+				distance: 3000000,
+			},
+			minRemainingBalance: "5000000",
+			activeDelegates: 101,
+			standbyDelegates: 2,
+			delegateListRoundOffset: 2,
+			unlockFixHeight: 16785878,
+			transferFixHeight: 17557156,
+			serializationFixHeight: 17557156,
+		},
+		registeredModules: [
+			{
+				id: 2,
+				name: "token",
+				actions: [],
+				events: [],
+				reducers: [
+					"token:credit",
+					"token:debit",
+					"token:getBalance",
+					"token:getMinRemainingBalance",
+				],
+				transactionAssets: [
+					{
+						id: 0,
+						name: "transfer",
+					},
+				],
+			},
+			{
+				id: 3,
+				name: "sequence",
+				actions: [],
+				events: [],
+				reducers: [],
+				transactionAssets: [],
+			},
+			{
+				id: 4,
+				name: "keys",
+				actions: [],
+				events: [],
+				reducers: [],
+				transactionAssets: [
+					{
+						id: 0,
+						name: "registerMultisignatureGroup",
+					},
+				],
+			},
+			{
+				id: 5,
+				name: "dpos",
+				actions: ["dpos:getAllDelegates", "dpos:getUnlockings"],
+				events: [],
+				reducers: [],
+				transactionAssets: [
+					{
+						id: 0,
+						name: "registerDelegate",
+					},
+					{
+						id: 1,
+						name: "voteDelegate",
+					},
+					{
+						id: 2,
+						name: "unlockToken",
+					},
+					{
+						id: 3,
+						name: "reportDelegateMisbehavior",
+					},
+				],
+			},
+			{
+				id: 1000,
+				name: "legacyAccount",
+				actions: ["legacyAccount:getUnregisteredAccount"],
+				events: [],
+				reducers: [],
+				transactionAssets: [
+					{
+						id: 0,
+						name: "reclaimLSK",
+					},
+				],
+			},
+		],
+		network: {
+			port: 8001,
+			seedPeers: [
+				{
+					ip: "mainnet-seed-01.lisk.io",
+					port: 8001,
+				},
+				{
+					ip: "mainnet-seed-02.lisk-nodes.net",
+					port: 8001,
+				},
+				{
+					ip: "mainnet-seed-03.lisk.io",
+					port: 8001,
+				},
+				{
+					ip: "mainnet-seed-04.lisk-nodes.net",
+					port: 8001,
+				},
+				{
+					ip: "mainnet-seed-05.lisk.io",
+					port: 8001,
+				},
+				{
+					ip: "mainnet-seed-06.lisk-nodes.net",
+					port: 8001,
+				},
+				{
+					ip: "mainnet-seed-07.lisk.io",
+					port: 8001,
+				},
+				{
+					ip: "mainnet-seed-08.lisk-nodes.net",
+					port: 8001,
+				},
+				{
+					ip: "mainnet-seed-09.lisk.io",
+					port: 8001,
+				},
+				{
+					ip: "mainnet-seed-10.lisk-nodes.net",
+					port: 8001,
+				},
+			],
+		},
+	},
+	meta: {},
+};
+
+module.exports = {
+	transactionsArray,
+	totalTransferValue,
+    currentBlockData,
+};
